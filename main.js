698f1d996a7d52ff04499bd219bcbbbd41a4d343
NB2010NB <natanbalai1@gmail.com>
2022-05-31 17:16:14 +0300

    dom

---
+++ main.js
@@ -0,0 +1,71 @@
+
+// ********1******* */
+
+// var H2 = document.getElementById("h2").innerText;
+// console.log(H2)
+// ******2****** */
+
+// var asaf = document.getElementById("TI").innerText = "firstDomApp";
+// console.log(asaf);
+
+// //******3***** ****/
+
+// var lol = document.getElementById("nai").innerText;
+// console.log(lol);
+
+// var lel = document.getElementById("nan").innerText;
+// console.log(lel);
+
+// var lool = document.getElementById("nam").innerText;
+// console.log(lool);
+
+// var lole = document.getElementById("nal").innerText;
+// console.log(lole);
+
+// ********4******** */
+// var lem = document.getElementById("nan").innerText;
+// console.log(lem);
+
+// **********5***********  */
+// var lom = document.getElementById("nam").innerText;
+// console.log(lom);
+
+// ********6******* */
+// var nel = document.getElementById("ele").innerText;
+// console.log(nel);
+
+// var no = document.getElementById("elem"); innerText;
+// console.log(no)
+
+// var nele = document.getElementById("elem"); innerText;
+// console.log(nele);
+
+// var neli = document.getElementById("eleme"); innerText;
+// console.log(neli);
+
+// var nol = document.getElementById("elemen"); innerText;
+// console.log(nol);
+
+//********7******* */
+
+// function printh2() {
+//     var wow = prompt("inter");
+//     var titleH2 = document.createElement('h2')
+//     titleH2.innerText = wow
+//     document.getElementById("container").appendChild(titleH2)
+
+// } 
+// printh2()
+
+//***********8********** */
+
+// function printColorNumber() {
+//     var nameColor = prompt("inter color");
+//     var numberName = +prompt("inter number");
+//     for (var i = 0; i < numberName; i++) {
+//         var span = document.createElement('span');
+//         span.innerHTML += nameColor + "<br>"
+//         document.querySelector('#container').appendChild(span, '<br>');
+//     }
+// }
+// printColorNumber()
